cdf1a43a8913af08fe806e9904979110950a947c
JungHamyung <jhm@mz.co.kr>
2020-07-21 08:06:18 +0900

    modiy

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
 
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoosu = require('mongoose')
 mongoosu.connect('mongodb+srv://jhm:wjdgkaud87!@cluster0.4rupp.mongodb.net/<dbname>?retryWrites=true&w=majority', {
@@ -11,5 +18,20 @@ mongoosu.connect('mongodb+srv://jhm:wjdgkaud87!@cluster0.4rupp.mongodb.net/<dbna
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+
+app.post('/register', (req, res) => {
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
+})
+
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
