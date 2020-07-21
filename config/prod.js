7e5231e670631c4fc49b32c17e2cb7f4a99d2b4e
JungHamyung <jhm@mz.co.kr>
2020-07-21 09:31:25 +0900

    login add

--- config/prod.js
+++ config/prod.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: process.env.MONGO_URI
+    //mongoURI: process.env.MONGO_URI
+    mongoURI: 'mongodb+srv://jhm:wjdgkaud87!@cluster0.4rupp.mongodb.net/<dbname>?retryWrites=true&w=majority'
 }
--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+
+const config = require('./config/key')
+
 const { User } = require("./models/User")
 
 //application/x-www-form-urlencoded
@@ -10,8 +14,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 //application/json
 app.use(bodyParser.json());
 
+app.use(cookieParser())
+
 const mongoosu = require('mongoose')
-mongoosu.connect('mongodb+srv://jhm:wjdgkaud87!@cluster0.4rupp.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoosu.connect(config.mongoURI, {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected.....'))
   .catch(err => console.log(err))
@@ -23,12 +29,36 @@ app.get('/', (req, res) => res.send('Hello World!'))
 app.post('/register', (req, res) => {
 
     const user = new User(req.body)
-
     user.save((err, userInfo) => {
         if(err) return res.json({ success: false, err})
         return res.status(200).json({
             success: true
         })
+    })
+})
+
+app.post('/login', (req, res) => {
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "Not exist User."
+            })
+        }
+
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if(!isMatch)
+                return res.json({loginSuccess: false, message: "Not matched password."})
+            user.generateToken((err, user) => {
+                if(err) return res.status(400).send(err);
+
+                res.cookie("x-auth", user.token)
+                .status(200)
+                .json({ loginSuccess: true,
+                userId: user._id})
+            })
+        })    
+
     })
 })
 
